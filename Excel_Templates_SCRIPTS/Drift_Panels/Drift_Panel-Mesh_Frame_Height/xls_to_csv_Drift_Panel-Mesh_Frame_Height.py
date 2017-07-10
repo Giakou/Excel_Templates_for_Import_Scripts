dde3dcd3824a71e126603914f1d8f3879265e5a4
Giakou <georgios.giakoustidis@cern.ch>
2017-07-10 12:24:44 +0300

    Add files via upload

--- Excel_Templates_SCRIPTS/Drift_Panels/Drift_Panel-Mesh_Frame_Height/xls_to_csv_Drift_Panel-Mesh_Frame_Height.py
+++ Excel_Templates_SCRIPTS/Drift_Panels/Drift_Panel-Mesh_Frame_Height/xls_to_csv_Drift_Panel-Mesh_Frame_Height.py
@@ -1,64 +1,37 @@
 # -*- coding: utf-8 -*-
 import xlrd
 from datetime import datetime
-
-workbook =  xlrd.open_workbook('Drift_Panel-Mesh_Frame_Height_5384_side_1.xls')
+#1) Excel_Measurements_Playground -> Save the excel file in the Playground dir
+#2) Excel_Measurements_Construction -> Save the excel file in the Construction dir
+#3) The directories are identical
+#4) CSV_files_Playground -> Save the data in csv form in the Playground dir
+#5) CSV_files_Construction -> Save the data in csv form in the Construction dir
+#6) The directories are identical
+#7) Don't forget to change the name of the excel and csv files by adding "_eqentryid" before the .xls or .csv
+workbook =  xlrd.open_workbook('/home/atlas-auth/Programs/ATLAS_NSW_AUTH_IMPORT_SCRIPTS/Excel_Measurements_Playground/Drift_Panels/Drift_Panel-Mesh_Frame_Height/Drift_Panel-Mesh_Frame_Height.xls')
 sheet = workbook.sheet_by_name('Sheet1')
-csvfile = open('Drift_Panel-Mesh_Frame_Height_5384_side_1.csv', 'wb')
+csvfile = open('/home/atlas-auth/Programs/ATLAS_NSW_AUTH_IMPORT_SCRIPTS/CSV_files_Playground/Drift_Panels/Drift_Panel-Mesh_Frame_Height/Drift_Panel-Mesh_Frame_Height.csv', 'wb')
+meascomment=sheet.cell(14,0).value
+meascommenttype=sheet.cell(17,0).value
+eqcomment=sheet.cell(20,0).value
+eqcommenttype=sheet.cell(23,0).value
 shifter=sheet.cell(11,0).value
 EQID=int(sheet.cell(5,0).value)
 date=sheet.cell(8,0).value
-position=sheet.cell(14,0).value
+position=sheet.cell(26,0).value
 datetuple=xlrd.xldate_as_tuple(date,workbook.datemode)
 fdate=datetime(*datetuple) #convert date in datetime type
 fdate=fdate.strftime("%Y-%m-%d %H:%M:%S") #date to str
 print "shifter, EQID,date",shifter,EQID,type(shifter),fdate
 resstr=""
-resstr+=";".join(['MEASSITEHASH','EQENTRYID','CONTEXTNAME','POSITION','MEASVALUE','PERCENTAGEMEAS','ISVALIDFLAG','INDEXHASH','MEASTIME','SHIFTER','WEBSITEUSERCR']) +"\n"
-resstr+=";".join(["",str(EQID),'DP_MESH_FR_HEIGHT_MIN_DT',position,str(sheet.cell(33,1).value),"","T",'skfh_1',fdate,shifter,'ggiakous']) +"\n"
-resstr+=";".join(["",str(EQID),'DP_MESH_FR_HEIGHT_MAX_DT',position,str(sheet.cell(33,3).value),"","T",'skfh_1',fdate,shifter,'ggiakous']) +"\n"
-resstr+=";".join(["",str(EQID),'DP_MESH_FR_HEIGHT_AVG_DT',position,str(sheet.cell(33,5).value),"","T",'skfh_1',fdate,shifter,'ggiakous']) +"\n"
-resstr+=";".join(["",str(EQID),'DP_MESH_FR_HEIGHT_RMS_DT',position,str(sheet.cell(33,7).value),"","T",'skfh_1',fdate,shifter,'ggiakous'])
+resstr+=";".join(['MEASSITEHASH','EQENTRYID','CONTEXTNAME','POSITION','MEASVALUE','PERCENTAGEMEAS','ISVALIDFLAG','INDEXHASH','MEASTIME','SHIFTER','WEBSITEUSERCR','MEASCOMMENT','MEASCOMMENTTYPE','EQCOMMENT','EQCOMMENTTYPE']) +"\n"
+resstr+=";".join(["",str(EQID),'DP_MESH_FR_HEIGHT_MIN_DT',position,str(sheet.cell(33,2).value),"","T",'skfh_1',fdate,shifter,'ggiakous',meascomment,meascommenttype,eqcomment,eqcommenttype]) +"\n"
+resstr+=";".join(["",str(EQID),'DP_MESH_FR_HEIGHT_MAX_DT',position,str(sheet.cell(33,4).value),"","T",'skfh_1',fdate,shifter,'ggiakous',meascomment,meascommenttype,eqcomment,eqcommenttype]) +"\n"
+resstr+=";".join(["",str(EQID),'DP_MESH_FR_HEIGHT_AVG_DT',position,str(sheet.cell(33,6).value),"","T",'skfh_1',fdate,shifter,'ggiakous',meascomment,meascommenttype,eqcomment,eqcommenttype]) +"\n"
+resstr+=";".join(["",str(EQID),'DP_MESH_FR_HEIGHT_RMS_DT',position,str(sheet.cell(33,8).value),"","T",'skfh_1',fdate,shifter,'ggiakous',meascomment,meascommenttype,eqcomment,eqcommenttype])
 
 
 csvfile.write(resstr)
 csvfile.close()
 
 pattern="%s;%s;"
-
-
-
-#############################################################
-
-#import xlrd
-#import os.path
-
-#wb = xlrd.open_workbook(os.path.join('/home/giakou/Desktop/MICROMEGAS_THESSALONIKI_MEASUREMENTS','Central_panel_planarity.xlsx'))
-#wb.sheet_names()
-#sh = wb.sheet_by_index(0)
-#i = 1
-#with open("Central_panel_planarity_and_thickness.txt", "a") as my_file:
-#    while sh.cell(i,11).value != 0:
-#        Load = sh.cell(i,11).value
- #       all_d = sh.col_values(i, 13, 19)
-  #      DB1 = Load + " " + (" ".join(all_d))
-   #     my_file.write(DB1 + '\n')
-    #    i += 1
-
-############################################################
-
-#def xls_to_csv(Central_panel_planarity.xls, Sheet3, Central_panel_planarity_and_thickness.csv):
- #    import xlrd
-  #   import csv
-   #  workbook = xlrd.open_workbook(Central_panel_planarity.xls)
-    # worksheet = workbook.sheet_by_name(Sheet3)
-     #csvfile = open(Central_panel_planarity_and_thickness.csv, 'wb')
-#     wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-
-#     for rownum in xrange(worksheet.nrows):
- #        wr.writerow(list(x.encode('utf-8') if type(x) == type(u'') else x
-  #                for x in worksheet.row_values(rownum))))
-
-   #  csvfile.close()
-
-#
